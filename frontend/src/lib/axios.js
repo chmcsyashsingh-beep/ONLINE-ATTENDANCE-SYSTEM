ec922ca4a06c24d9a262f00fd91df39e1a72ea62
chmcsyashsingh-beep <chmcs.yash.singh@gmail.com>
2026-03-10 20:06:45 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/attendance'
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/attendance"
+      : "https://online-attendance-system-tf94.onrender.com/attendance",
 });
 
 export default api;
